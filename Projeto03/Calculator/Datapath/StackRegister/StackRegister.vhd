1d4f07c36800cc75b5c3b7a5f677ed18a7fd6a0f
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2022-12-12 00:01:37 -0300

    corrige funcionamento do registrador da stack

--- Projeto03/Calculator/Datapath/StackRegister/StackRegister.vhd
+++ Projeto03/Calculator/Datapath/StackRegister/StackRegister.vhd
@@ -1,25 +1,70 @@
+-- O proprio registrador da stack e uma FSM
 
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
 
 entity StackRegister is
   port (
     X : in std_logic_vector(3 downto 0);
-    load, clear : in std_logic;
+    load, clear, clk : in std_logic;
     isEmpty : out std_logic;
-    Y : out std_logic_vector(3 downto 0)
+    Z : out std_logic_vector(3 downto 0)
   );
 end StackRegister;
 
 architecture arch_StackRegister of StackRegister is
-  signal Y_interm : std_logic_vector(3 downto 0);
-begin
-  Y_interm <=
-    "0000" when clear = '1' else
-    X when load = '1' else
-    Y_interm;
+  type state_type is (HOLD_ST, LOAD_ST, CLEAR_ST);
+  attribute ENUM_ENCODING : string;
+  attribute ENUM_ENCODING of state_type : type is "00 01 10";
+  signal PS, NS : state_type;
 
-  isEmpty <= clear;
-  Y <= Y_interm;
+  signal Y : std_logic_vector(1 downto 0); -- para os estados da FSM
+begin
+  sync_process : process (CLK, NS)
+  begin
+    if (rising_edge(CLK)) then
+      PS <= NS;
+    end if;
+  end process sync_process;
+
+  comb_process : process (PS, load, clear, X)
+  begin
+    
+    case PS is
+
+      when HOLD_ST =>
+        if (clear = '1') then
+          NS <= CLEAR_ST;
+        elsif (load = '1') then
+          NS <= LOAD_ST;
+        else
+          NS <= HOLD_ST;
+        end if;
+
+      when LOAD_ST =>
+        Z <= X;
+        isEmpty <= '0'; 
+        NS <= HOLD_ST;
+
+      when CLEAR_ST =>
+        Z <= "0000";
+        isEmpty <= '1'; 
+
+        if (clear = '0') then
+          NS <= HOLD_ST;
+        else 
+          NS <= CLEAR_ST;
+        end if;
+
+      when others => -- catch all, nunca deve chegar aqui
+        NS <= HOLD_ST;
+    end case;
+  end process comb_process;
+
+  -- codificação dos estados Y
+  with PS select
+    Y <= "00" when HOLD_ST,
+    "01" when LOAD_ST,
+    "10" when CLEAR_ST,
+    "00" when others;
 end arch_StackRegister;
--- Projeto03/Calculator/Datapath/StackRegister/tb_StackRegister.vhd
+++ Projeto03/Calculator/Datapath/StackRegister/tb_StackRegister.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
+use ieee.numeric_std.all;
 
 entity tb_StackRegister is
 end tb_StackRegister;
@@ -9,26 +10,52 @@ architecture arch_tb_StackRegister of tb_StackRegister is
   component StackRegister is
     port (
       X : in std_logic_vector(3 downto 0);
-      load, clear : in std_logic;
+      load, clear, clk : in std_logic;
       isEmpty : out std_logic;
-      Y : out std_logic_vector(3 downto 0)
+      Z : out std_logic_vector(3 downto 0)
     );
   end component;
 
+  constant PERIODO : time := 20 ns; -- 50 MHz
+  signal ENT_CLK : std_logic := '0'; -- deve ser inicializado
+  signal ENT_CLK_ENABLE : std_logic := '1'; -- Sinal de Enable do clock só para efeito de controle do fim da simulação
+
   signal tb_X : std_logic_vector(3 downto 0);
   signal tb_load, tb_clear, tb_isEmpty : std_logic;
-  signal tb_Y : std_logic_vector(3 downto 0);
+  signal tb_Z : std_logic_vector(3 downto 0);
+
 begin
+  -- geração do clock com periodo PERIODO
+  ENT_CLK <= ENT_CLK_ENABLE and not ENT_CLK after PERIODO/2;
+  ENT_CLK_ENABLE <= '1', '0' after 40 * PERIODO; -- a simulação termina após transcorrer 20 períodos de clock.
+
   DUT : StackRegister port map(
+    clk => ENT_CLK,
     X => tb_X,
     load => tb_load,
     clear => tb_clear,
     isEmpty => tb_isEmpty,
-    Y => tb_Y
+    Z => tb_Z
   );
 
-  tb_clear <= '1', '0' after 2 ns;
-  tb_load <= '0', '1' after 22 ns;
-  tb_X <= "1101", "0010" after 20 ns, "0010" after 40 ns;
-  -- tb_lo
+  --  process para os estímulos das entradas
+  stimulus : process
+  begin
+    tb_load <= '1';
+    tb_clear <= '0';
+    tb_X <= "0010";
+    wait for 10 * PERIODO;
+    tb_load <= '0';
+    tb_clear <= '1';
+    wait for 2 * PERIODO;
+    tb_X <= "1010";
+    wait for 10 * PERIODO;
+    tb_clear <= '0';
+    wait for 5 * PERIODO;
+    tb_load <= '1';
+    wait for 5 * PERIODO;
+    tb_X <= "1111";
+
+    wait;
+  end process stimulus;
 end arch_tb_StackRegister;
